be7f90df31a2d8c6256c46ce1866c4327808a7f9
EmilyErickson <emilyerickson2002@gmail.com>
2023-01-26 11:39:54 -0600

    Create UI for rock paper scissors

--- javascript.js
+++ javascript.js
@@ -1,80 +1,98 @@
 
+let playerChoice = function (winner) {
+    document.querySelector(".winner").textContent = message;
+};
 
-let rock = "rock"
-let paper = "paper"
-let scissors = "scissors"
+let scoreChart = function (score) {
+    document.querySelector(".score").textContent = message;
+  };
+
+
+let rock = document.querySelector("#rock").addEventListener("click", gameRound)
+let paper = document.querySelector("#paper").addEventListener("click", gameRound)
+let scissors = document.querySelector("#scissors").addEventListener("click", gameRound)
+
+
+let restart = document.querySelector("#restart").addEventListener("click", restartGame)
+
+let playerSelection = this.id
+
 let playerScore = 0
 let computerScore = 0
+
+document.querySelector('.winner').textContent = "Who will win?"
+
+document.querySelector('.score').textContent = `Your score = ${playerScore}   ` + `\nComputer score =  ${computerScore}`
+
+
+let computerSelection = Math.random()
+
 function gameRound () {
 
-    let computerSelection = Math.random()
 
-    let playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+    if (playerScore < 5 && computerScore < 5) {
 
+        let computerSelection = Math.random()
 
-    switch (playerSelection) {
-        case rock:
-            if (computerSelection >=0.67) {
-                playerScore += 1
-                return ("You Win! Rock beats Scissors")
-            }else if (computerSelection >=0.34) {
-                computerScore +=1
-                return("You Lose! Paper beats Rock")
-            }else if (computerSelection <0.34) {
-                return ("It's a draw!")
-            }
-            break;
+        let playerSelection = this.id
+        
+        switch (playerSelection) {
+            case "rock":
+                if (computerSelection >=0.67) {
+                    playerScore += 1
+                    document.querySelector('.winner').textContent =  "You Win! Rock beats Scissors"
+                }else if (computerSelection >=0.34) {
+                    computerScore +=1
+                    document.querySelector('.winner').textContent ="You Lose! Paper beats Rock"
+                }else if (computerSelection <0.34) {
+                    document.querySelector('.winner').textContent ="It's a draw!"
+                }
+                break;
 
-        case paper:
-            if (computerSelection <0.34) {
-                playerScore += 1
-                return ("You Win! Paper beats Rock")
-            }else if (computerSelection >=0.67) {
-                computerScore += 1
-                return("You Lose! Scissor beats Paper")
-            }else if (computerSelection >=0.34) {
-                return ("It's a draw!")
-            }
-            break;
+            case "paper":
+                if (computerSelection <0.34) {
+                    playerScore += 1
+                    document.querySelector('.winner').textContent = "You Win! Paper beats Rock"
+                }else if (computerSelection >=0.67) {
+                    computerScore += 1
+                    document.querySelector('.winner').textContent = "You Lose! Scissor beats Paper"
+                }else if (computerSelection >=0.34) {
+                    document.querySelector('.winner').textContent = "It's a draw!"
+                }
+                break;
 
-        case scissors:
-            if (computerSelection >=0.34) {
-                playerScore += 1
-                return ("You Win! Scissors beats Paper")
-            }else if (computerSelection <0.34) {
-                computerScore +=1
-                return("You Lose! Rock beats Scissors")
-            }else if (computerSelection >=0.67) {
-                return ("It's a draw!")
-            }
-            break;
-           
-        default:
-            return("Something went wrong")
+            case "scissors":
+                if (computerSelection >=0.34) {
+                    playerScore += 1
+                    document.querySelector('.winner').textContent = "You Win! Scissors beats Paper"
+                }else if (computerSelection <0.34) {
+                    computerScore +=1
+                    document.querySelector('.winner').textContent ="You Lose! Rock beats Scissors"
+                }else if (computerSelection >=0.67) {
+                    document.querySelector('.winner').textContent = "It's a draw!"
+                }
+                break;
             
+            default:
+                document.querySelector('.winner').textContent = "Something went wrong"
+                
+        }
+
+    document.querySelector('.score').textContent = `Your score = ${playerScore}   ` + `\nComputer score =  ${computerScore}`
     }
-    return (`Computer = ${computerScore}`, `Player = ${playerScore}`)
+
+    if (playerScore == 5) {
+        document.querySelector('.winner').textContent = "GAME OVER! \nYOU WON!!"
+    } else if (computerScore == 5) {
+        document.querySelector('.winner').textContent = "GAME OVER! \nYOU LOST!"
+    }
+
 }
 
+function restartGame () {
+    playerScore = 0
+    computerScore = 0
 
-function game() {
-    
-    for (let i = 0; i < 5; i++) {
-
-        console.log(gameRound())
-        console.log ("round " + (i+1))
-        console.log (`Computer = ${computerScore}`, `Player = ${playerScore}`)
-    }
+    document.querySelector('.winner').textContent = "Who will win?"
+    document.querySelector('.score').textContent = `Your score = ${playerScore}   ` + `\nComputer score =  ${computerScore}`
 }
-    function score() {
-        if (computerScore > playerScore) {
-        return ("You lost the game")
-    } else if (computerScore < playerScore) {
-        return ("You won the game")
-    } else {
-        return ("The game is a draw")
-    }
-}
-
-game()
-console.log(score())
